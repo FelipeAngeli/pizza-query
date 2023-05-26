86566c78dc8efee593deb2af8e02987bae19d06c
felipeangeli <felipeangeli>
2023-05-26 23:29:26 +0000

    aula 3

---
+++ dia 03/JOIN.sql
@@ -0,0 +1,35 @@
+-- Databricks notebook source
+SELECT t1.*,
+        t2.vlPreco
+FROM silver.pizza_query.item_pedido AS t1
+
+LEFT JOIN silver.pizza_query.produto AS t2
+ON t1.descItem = t2.descItem
+
+
+-- COMMAND ----------
+
+SELECT t1.idPedido,
+        sum(t2.vlPreco) AS vlPedido
+FROM silver.pizza_query.item_pedido AS t1
+
+LEFT JOIN silver.pizza_query.produto AS t2
+ON t1.descItem = t2.descItem
+
+WHERE t1.descTipoItem <> 'bebida'
+
+GROUP BY t1.idPedido
+
+
+-- COMMAND ----------
+
+SELECT t1.idPedido,
+        sum(t2.vlPreco) AS vlPedido
+FROM silver.pizza_query.item_pedido AS t1
+
+LEFT JOIN silver.pizza_query.produto AS t2
+ON t1.descItem = t2.descItem
+
+WHERE t1.descTipoItem <> 'bebida'
+
+GROUP BY t1.idPedido
